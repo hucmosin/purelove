9289ad889efec6d6b4ba1f2be316384861a62de7
hucmosin <hucmoxing@163.com>
2017-07-27 11:31:58 +0800

    purelove run

---
+++ PureLove-Win/PureLove.bat
@@ -0,0 +1,4 @@
+@echo off
+SET purelove_ROOT=%CD%
+
+start %CD%/ConEmu/ConEmu.exe /Icon "%purelove_ROOT%\ConEmu\icons\PureLove.ico" /Title "PureLove" /LoadCfgFile "%purelove_ROOT%\ConEmu\config\ConEmu.xml"
