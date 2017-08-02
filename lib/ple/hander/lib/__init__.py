f4a2824b1bc6dac57b7dd864c882f33b5268e0f6
hucmosin <hucmoxing@163.com>
2017-08-02 13:45:17 +0800

    hander lib

--- lib/ple/hander/lib/__init__.py
+++ lib/ple/hander/lib/__init__.py
@@ -1,1 +1,14 @@
-__all__ = [ 'crypto', 'persistence', 'scan', 'survey', 'toolkit' ]
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+author: mosin <hucmoxing@163.com>
+"""
+
+import os
+import sys
+
+pwd = pwd.split('Purelove')[0]
+pwd = pwd +"/Purelove"
+sys.path.append(os.path.join(os.path.dirname(__file__), pwd )
+
