720a4131a876c5feca905afc6cd57b261196893d
hucmosin <hucmoxing@163.com>
2017-09-06 10:47:23 +0800

    banner

--- api/banner.py
+++ api/banner.py
@@ -2,25 +2,25 @@
 # -*- coding: utf-8 -*-
 #导入model数量
 import pl_print_world_color as color
+from utils.payload_count import ret_num
 
 
 
-
+payloads = ret_num()
 #读取pl里的payload数量
-def pay_count():
-    return 
-
 def pl_get_banner():
     #打开缓存文件，取出文件个数
     #poc_count = pl_get_poc_count()
     desc = '''
 
-       =[               purelove v1.1.0	                     ]
-+ -- --=[                                                    ]
-+ -- --=[                  by mosin                          ]
-+ -- --=[    email: hucmoxing@163.com                        ]
-+ -- --=[    Guthub Url: https://github.com/hucmosin         ]
-'''  #.format(payload = color.set_yellow())
+       =[               purelove v1.1.0	                              ]
++ -- --=[                                                             ]
++ -- --=[                  by mosin                                   ]
++ -- --=[                 email: hucmoxing@163.com                    ]
++ -- --=[    Guthub Url: https://github.com/hucmosin/purelove         ]
++ -- --=[    Shellcode - (0) Different OS                             ]
++ -- --=[    Payload   - ({payload}) (Payloads and Exploits Module)   ]
+'''.format(payload = color.set_yellow(payloads))
     print desc
 
 
